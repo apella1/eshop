92e54e82be86538f04de4cc34649560fa5e078fe
apella1 <johncollons5@gmail.com>
2024-05-01 19:42:24 +0300

    feat: add product controller and order controller

--- src/main/java/me/apella/eshop/controller/OrderController.java
+++ src/main/java/me/apella/eshop/controller/OrderController.java
@@ -1,4 +1,61 @@
 package me.apella.eshop.controller;
 
+import me.apella.eshop.model.Order;
+import me.apella.eshop.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> orders = orderService.findAll();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody Order order) {
+        orderService.createOrder(order);
+        return new ResponseEntity<>("Order created successfully!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable UUID orderId) {
+        Order order = orderService.getOrderById(orderId);
+
+        if (order != null) {
+            return new ResponseEntity<>(order, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<String> deleteOrderById(@PathVariable UUID orderId) {
+        boolean deleted = orderService.deleteOrderById(orderId);
+        if (deleted) {
+            return new ResponseEntity<>("Order deleted successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Order not found!", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<String> updateOrderById(@PathVariable UUID orderId, @RequestBody Order order) {
+        boolean updated = orderService.updateOrderById(orderId, order);
+        if (updated) {
+            return new ResponseEntity<>("Order updated successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Order not found!", HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/me/apella/eshop/controller/ProductController.java
+++ src/main/java/me/apella/eshop/controller/ProductController.java
@@ -1,4 +1,60 @@
 package me.apella.eshop.controller;
 
+import me.apella.eshop.model.Product;
+import me.apella.eshop.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> products = productService.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody Product product) {
+        productService.createProduct(product);
+        return new ResponseEntity<>("Product created successfully!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable UUID productId) {
+        Product product = productService.getProductById(productId);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteProductById(@PathVariable UUID productId) {
+        boolean deleted = productService.deleteProductById(productId);
+        if (deleted) {
+            return new ResponseEntity<>("Product deleted successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Product not found!", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<String> updateProductById(@PathVariable UUID productId, @RequestBody Product product) {
+        boolean updated = productService.updateProductById(productId, product);
+        if (updated) {
+            return new ResponseEntity<>("Product updated successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Product not found!", HttpStatus.NOT_FOUND);
+    }
 }
